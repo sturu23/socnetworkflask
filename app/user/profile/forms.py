9672b9d74489fc747a28ad93f1a2b5f0603d6bc7
Your Name <you@example.com>
2022-08-30 15:09:20 +0400

    added profile things, proffesion,skills and username

--- app/user/profile/forms.py
+++ app/user/profile/forms.py
@@ -8,16 +8,17 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextA
 
 
 class UploadPhotoForm(FlaskForm):
-
     photo = FileField('Upload a photo')
-    submit = SubmitField('Upload')
+    submit2 = SubmitField('Upload')
+
 
 
 class RemovePhotoForm(FlaskForm):
     submit = SubmitField('Remove')
 
+
 class UpdateForm(FlaskForm):
     username = StringField('Name',validators=[DataRequired(), Length(min=2, max=20)])
-    proffesion = StringField('Proffesion',validators=[DataRequired(), Length(min=2, max=20)])
-    skills = StringField('Skills',validators=[DataRequired(), Length(min=2, max=20)])
+    proffesion = StringField('Proffesion',validators=[DataRequired(), Length(min=2, max=50)])
+    skills = StringField('Skills',validators=[DataRequired(), Length(min=2, max=100)])
     submit1 = SubmitField('Update')
--- app/user/profile/views.py
+++ app/user/profile/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 from app.user.profile.forms import UploadPhotoForm, RemovePhotoForm,UpdateForm
 from werkzeug.utils import secure_filename
-from app.models import db,User
+from app.models import db,User,EditProfile
 import uuid
 import os
 
@@ -13,13 +13,26 @@ profile_blueprint = Blueprint('profile', __name__,template_folder='templates')
 @profile_blueprint.route('/profile',methods=['GET','POST'])
 @login_required
 def profile():
+    #forms
     form = UploadPhotoForm()
+    uform = UpdateForm()
+    #forms
 
+    #querys
     user_posts = User.query.filter_by(id=current_user.id).first()
     user_post = user_posts.statia
+
+    user_all = EditProfile.query.filter_by(id=current_user.id).first()
+
+
+
+
+    #querys
     data = []
 
-    if form.validate_on_submit():
+    if form.submit2.data and form.validate():
+        #for picture upload
+
         photo = form.photo.data
         photo_secure = secure_filename(photo.filename)
         pic_uid = str(uuid.uuid1()) + '_' + photo_secure
@@ -30,6 +43,30 @@ def profile():
         db.session.commit()
         flash('Photo uploaded successfully!')
 
+    if uform.submit1.data and uform.validate():
+
+        #for profile update
+        username = uform.username.data
+        proffesion = uform.proffesion.data
+        skills = uform.skills.data
+        splited_skills = skills.split(',')
+
+
+        user = EditProfile.query.filter_by(id=current_user.id).first()
+        if user:
+            user.username = username
+            user.proffesion = proffesion
+            user.skills = skills
+            user.id = current_user.id
+            db.session.commit()
+        else:
+            db_edit = EditProfile(username=username,proffesion=proffesion,skills=skills,user_id=current_user.id)
+            db.session.add(db_edit)
+            db.session.commit()
+
+
+
+
     for post in user_post:
         data.append({
             'id': post.id,
@@ -39,22 +76,9 @@ def profile():
             'created_post_date': post.created_post_date,
         })
 
-    return render_template('profile.html',form=form,user_post=user_post)
+    return render_template('profile.html',form=form,user_post=user_post,uform=uform,user_info=user_all)
 
 
 
-@login_required
-def update_profile():
-    form = UpdateForm()
-
-
-    if form.validate_on_submit():
-        name = form.name.data
-        proffesion = form.proffesion.data
-        skills = form.skills.data
-
-        print(name,proffesion,skills)
-
-    return render_template('profile.html',form=form)
 
 
